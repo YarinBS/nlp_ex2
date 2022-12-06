23e31df9e1bae848b840199f53b3c07e32da24eb
pereztomer <pereztomer3@gmail.com>
2022-12-06 14:53:32 +0200

    sdfsdfsdf

--- create_embedding.py
+++ create_embedding.py
@@ -1,13 +1,14 @@
-from gensim import downloader
+from gensim import downloader, models
 import numpy as np
+import os
+from gensim.models import KeyedVectors
+from gensim.downloader import base_dir
 
 
 def load_model():
     # WORD_2_VEC_PATH = 'word2vec-google-news-300'
     GLOVE_PATH = 'glove-twitter-200'
     glove = downloader.load(GLOVE_PATH)
-    glove.save("glove.model")
-
     return glove
 
 
@@ -25,9 +26,13 @@ def embed(model, sen):
 
 def main():
     sen = "i am a student at the technion"
+    # glove_model_path = '/home/user/gensim-data/glove-twitter-200/glove-twitter-200.gz'
+    # glove_model = KeyedVectors.load_word2vec_format(glove_model_path)
+
     glove_model = load_model()
     vec = embed(glove_model, sen)
     print(vec)
+
 
 if __name__ == '__main__':
     main()
