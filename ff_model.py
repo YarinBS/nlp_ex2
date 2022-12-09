060bc0cfb426f1cb4503c18143f46a944351336b
pereztomer <pereztomer3@gmail.com>
2022-12-09 12:57:07 +0200

    creating smv model

--- ff_model.py
+++ ff_model.py
@@ -2,80 +2,96 @@ import torch
 from torch import nn
 from sklearn.metrics import accuracy_score
 from torch.utils.data import DataLoader
+from parser import parse_file
+from create_embedding import load_model, generate_ds
+import torch.nn.functional as F
 
 
-class SentimentNN(nn.Module):
+class Net(nn.Module):
     def __init__(self, vec_dim, num_classes, hidden_dim=100):
-        super(SentimentNN, self).__init__()
+        super().__init__()
         self.first_layer = nn.Linear(vec_dim, hidden_dim)
         self.second_layer = nn.Linear(hidden_dim, num_classes)
         self.activation = nn.ReLU()
-        self.loss = nn.CrossEntropyLoss()
+        self.out_func = nn.Sigmoid()
 
-    def forward(self, input_ids, labels=None):
-        x = self.first_layer(input_ids)
+    def forward(self, x):
+        x = self.first_layer(x)
         x = self.activation(x)
         x = self.second_layer(x)
-        if labels is None:
-            return x, None
-        loss = self.loss(x, labels)
-        return x, loss
+        x = self.out_func(x)
+        return x
 
 
 # ---------------------
-def train(model, data_sets, optimizer, num_epochs: int, batch_size=16):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    data_loaders = {"train": DataLoader(data_sets["train"], batch_size=batch_size, shuffle=True),
-                    "test": DataLoader(data_sets["test"], batch_size=batch_size, shuffle=False)}
-    model.to(device)
+def train(model, train_ds, validation_ds, optimizer, num_epochs: int):
+    criterion = nn.CrossEntropyLoss()
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
 
-    best_acc = 0.0
+        running_loss = 0.0
+        for i, data in enumerate(train_ds, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            inputs = inputs.to(torch.float32)
+            labels = labels.to(torch.float32)
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-    for epoch in range(num_epochs):
-        print(f'Epoch {epoch + 1}/{num_epochs}')
-        print('-' * 10)
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-        for phase in ['train', 'test']:
-            if phase == 'train':
-                model.train()
-            else:
-                model.eval()
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
 
-            running_loss = 0.0
-            labels, preds = [], []
+    print('Finished Training')
 
-            for batch in data_loaders[phase]:
-                batch_size = 0
-                for k, v in batch.items():
-                    batch[k] = v.to(device)
-                    batch_size = v.shape[0]
 
-                optimizer.zero_grad()
-                if phase == 'train':
-                    outputs, loss = model(**batch)
-                    loss.backward()
-                    optimizer.step()
-                else:
-                    with torch.no_grad():
-                        outputs, loss = model(**batch)
-                pred = outputs.argmax(dim=-1).clone().detach().cpu()
-                labels += batch['labels'].cpu().view(-1).tolist()
-                preds += pred.view(-1).tolist()
-                running_loss += loss.item() * batch_size
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, x, y, num_classes):
+        self.x = x
+        self.y = y
+        self.num_classes = num_classes
 
-            epoch_loss = running_loss / len(data_sets[phase])
-            epoch_acc = accuracy_score(labels, preds)
+    def __len__(self):
+        return len(self.x)
 
-            epoch_acc = round(epoch_acc, 5)
+    def __getitem__(self, idx):
+        sample = self.x[idx]
+        label = torch.zeros(self.num_classes)
+        label[self.y[idx]] = 1
+        return sample, label
 
-            if phase.title() == "test":
-                print(f'{phase.title()} Loss: {epoch_loss:.4e} Accuracy: {epoch_acc}')
-            else:
-                print(f'{phase.title()} Loss: {epoch_loss:.4e} Accuracy: {epoch_acc}')
-            if phase == 'test' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                with open('model.pkl', 'wb') as f:
-                    torch.save(model, f)
-        print()
 
-    print(f'Best Validation Accuracy: {best_acc:4f}')
+def main():
+    train_file_path = r'./data/train.tagged'
+    validation_file_path = r'./data/dev.tagged'
+    windows_size = 2
+    glove_model = load_model()
+    train_set = parse_file(file_path=train_file_path,
+                           windows_size=windows_size)
+
+    validation_set = parse_file(file_path=validation_file_path,
+                                windows_size=windows_size)
+
+    x_train, y_train = generate_ds(glove_model, train_set)
+    x_validation, y_validation = generate_ds(glove_model, validation_set)
+
+    train_ds = DataLoader(CustomDataset(x=x_train, y=y_train), batch_size=16, shuffle=True)
+    validation_ds = DataLoader(CustomDataset(x=x_validation, y=y_validation), batch_size=16, shuffle=False)
+
+    model = Net(vec_dim=(windows_size * 2 + 1) * 200, num_classes=2, hidden_dim=100)
+    train(model=model,
+          train_ds=train_ds,
+          validation_ds=validation_ds,
+          optimizer=torch.optim.Adam(model.parameters(), 1e-4),
+          num_epochs=100)
+
+
+if __name__ == '__main__':
+    main()
